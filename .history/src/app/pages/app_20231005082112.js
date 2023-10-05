391c0675ff4c42cab4d1516c6c2786ad71d776f4
EncinasDev <encinas.jhonatan.dev@gmail.com>
2023-10-05 08:31:20 -0400

    feat: Agregar pagina de registro a home

---
+++ .history/src/app/pages/app_20231005082112.js
@@ -0,0 +1,27 @@
+const routes = {
+  "/": "home.html", // Página de inicio
+  registro: "registro.html",
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuItems = document.querySelectorAll(".menu__item");
+  const pageContent = document.getElementById("page-content");
+
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", function () {
+      const pageName = this.getAttribute("data-page");
+      loadPage(pageName);
+    });
+  });
+
+  function loadPage(pageName) {
+    fetch(`pages/html/${pageName}.html`)
+      .then((response) => response.text())
+      .then((html) => {
+        pageContent.innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error al cargar la página:", error);
+      });
+  }
+});
