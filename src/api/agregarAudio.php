c1c3c711a7bd953845956c8345afe606003f20cd
Kevin <kevindboy96@gmail.com>
2023-10-05 22:51:55 -0400

    arreglo del backend agregaraudio

--- src/api/agregarAudio.php
+++ src/api/agregarAudio.php
@@ -1,26 +1,43 @@
 <?php
-include('conexion.php');
+include("conexion.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-function agregarAudio($titulo_audio, $musica_fondo, $procedencia, $formato_audio, $tipo_audio, $narrador, $duracion, $archivo_audio_path, $archivo_texto_path) {
+    $titulo_audio = $_POST["titulo_audio"];
+    $musica_fondo = $_POST["musica_fondo"];
+    $procedencia = $_POST["procedencia"];
+    $formato_audio = $_POST["formato_audio"];
+    $tipo_audio = $_POST["tipo_audio"];
+    $narrador = $_POST["narrador"];
+    $duracion = $_POST["duracion"];
+
+    $archivo_audio_tmp = $_FILES["archivo_audio"]["tmp_name"];
+    $archivo_texto_tmp = $_FILES["archivo_texto"]["tmp_name"];
+
+    if (empty($archivo_audio_tmp) || empty($archivo_texto_tmp)) {
+        die("Error: Debes cargar ambos archivos de audio y texto.");
+    }
+
+    $archivo_audio = file_get_contents($archivo_audio_tmp);
+    $archivo_texto = file_get_contents($archivo_texto_tmp);
+
     $conn = conectarDB();
 
-    $archivo_audio = file_get_contents($archivo_audio_path);
-    $archivo_texto = file_get_contents($archivo_texto_path);
+    $consulta = "INSERT INTO audio (titulo_audio, musica_fondo, procedencia, formato_audio, tipo_audio, narrador, duracion, archivo_audio, archivo_texto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    $sql = "INSERT INTO audio (titulo_audio, musica_fondo, procedencia, formato_audio, tipo_audio, narrador, duracion, archivo_audio, archivo_texto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param($titulo_audio, $musica_fondo, $procedencia, $formato_audio, $tipo_audio, $narrador, $duracion, $archivo_audio, $archivo_texto);
+    if ($stmt = $conn->prepare($consulta)) {
+
+        $stmt->bind_param("sssbbb",$titulo_audio, $musica_fondo, $procedencia, $formato_audio, $tipo_audio, $narrador, $duracion, $archivo_audio, $archivo_texto);
 
         if ($stmt->execute()) {
-            echo "Registro insertado con éxito.";
+            echo "Los datos se han insertado correctamente en la base de datos.";
         } else {
-            echo "Error al insertar el registro: " . $stmt->error;
+            die("Error al insertar datos en la base de datos: " . $stmt->error);
         }
 
         $stmt->close();
     } else {
-        echo "Error en la preparación de la sentencia: " . $conn->error;
+        die("Error al preparar la sentencia SQL: " . $conn->error);
     }
 
     $conn->close();
--- src/api/conexion.php
+++ src/api/conexion.php
@@ -5,13 +5,14 @@ function conectarDB() {
     $password = "";
     $database = "qubit";
 
+    // Crea una conexión a la base de datos
     $conn = new mysqli($servername, $username, $password, $database);
 
+    // Verifica si la conexión tuvo éxito
     if ($conn->connect_error) {
         die("Error en la conexión: " . $conn->connect_error);
-    }else {
-        echo "Conexión exitosa a la base de datos.";
     }
+
     return $conn;
 }
 ?>
