aa94e4cb13e3439039299c2ccdc62ba442297281
Gabriel <117124925+Gabriel-Cabero-Torrico@users.noreply.github.com>
2023-10-10 11:40:56 -0400

    Boton eliminar

--- src/app/homeAdm/homeAdm.js
+++ src/app/homeAdm/homeAdm.js
@@ -1,26 +1,42 @@
-const numberCuentos = 4;
-const numeroLeyendas = 4;
-const scrollObject = document.querySelector('.contenidoUno');
-const DosScrollObject = document.querySelector('.contenidoUno');
 
-function agregarCuento(){
-    const contenidoCinco = document.createElement("h2");
-    contenidoCinco.innerHTML = 'Titulo de audio<br>Procedencia<br>Nombre del narrador<br>Nombre musica de fondo<br><br><br>'
-    scrollObject.appendChild(contenidoCinco);
+
+const boton1 = document.querySelector(".boton1");
+boton1.addEventListener("click", hideConfirma);
+
+const botones = document.querySelector(".boton");
+botones.addEventListener("click", hideConfirma);
+
+const deleteCs = document.querySelectorAll(".deleteC");
+for(let i = 0 ;i<=deleteCs.length;i++){
+    deleteCs[i].addEventListener("click",confirmar);
+
 }
-function eliminarCuento(){
-    const contenidoUno = document.querySelector('#conenidoUno');
-    scrollObject.removeChild(contenidoUno);
+function confirmar(){    
+    document.getElementById('confirmacion').style.display = 'block';
+    document.getElementById('header').style.background = "#3C3C3C";
+    document.getElementById('menu').style.background ="#3C3C3C";
+    document.getElementById('contenedor').style.background ="#3C3C3C";
+    document.getElementById('scrollBg').style.background ="#3C3C3C";
+    document.getElementById('DosScrollBg').style.background ="#3C3C3C";
+    document.getElementById('page-content').style.background ="#3C3C3C";
+
 }
-function agregarLeyenda(){
-    const contenidoCinco = document.createElement("h2");
-    contenidoCinco.innerHTML = 'Titulo de audio<br>Procedencia<br>Nombre del narrador<br>Nombre musica de fondo<br><br><br>'
-    scrollObject.appendChild(contenidoCinco);
+
+function hideConfirma(){    
+    document.getElementById('confirmacion').style.display = 'none';
+    document.getElementById('header').style.background ="linear-gradient(to top, #43cea2, #185a9d)";
+    document.getElementById('menu').style.background ="linear-gradient(to top, #43cea2, #185a9d)";
+    document.getElementById('contenedor').style.background ="linear-gradient(to top, #43cea2, #185a9d)";
+    document.getElementById('scrollBg').style.background ="linear-gradient(to top, #43cea2, #185a9d)";
+    document.getElementById('DosScrollBg').style.background ="linear-gradient(to top, #43cea2, #185a9d)";
+    document.getElementById('page-content').style.background ="linear-gradient(to top, #43cea2, #185a9d)";
+
+
+
 }
-function eliminarLeyenda(){
-    const contenidoCinco = document.createElement("h2");
-    contenidoCinco.innerHTML = 'Titulo de audio<br>Procedencia<br>Nombre del narrador<br>Nombre musica de fondo<br><br><br>'
-    scrollObject.appendChild(contenidoCinco);
+
+function subir(){
+
 }
 
 
