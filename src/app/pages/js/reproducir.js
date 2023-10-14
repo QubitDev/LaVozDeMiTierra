246981e7c286064727ec1af405db972e03fbd6bb
Yesica <sanchezmaria2c@gmail.com>
2023-10-14 14:09:13 -0400

    feat: modificacion de archivos

--- src/app/pages/js/reproducir.js
+++ src/app/pages/js/reproducir.js
@@ -1,19 +1,15 @@
-import { datos } from "./registro.js";
+//const doc = document.getElementById('id-doc').value;
 
-const datos = JSON.parse(sessionStorage.getItem("misDatos"));
-if (datos) {
-  // Usa los datos como necesites
-  console.log(datos);
-  console.log();
-}
+db.collection("audio").get().then((querySnapshot) =>{
+   querySnapshot.forEach((doc) => {
+      document.getElementById("datos_Audio").innerHTML=`<br><br><h3> ${doc.data().tipoAudio}</h3> 
+      <br>
+      <p>${doc.data().titulo}</p>
+      <p> Narrado por: ${doc.data().narrador}</p>
+      <p> Musica: ${doc.data().musica}</p>`;
 
-Object.assign(datos.arguments,{titulo, musica, procedencia, formato, tipoAudio, narrador, duracion});
-function datos(){
-console.log(datos.titulo);
-console.log(datos.musica);
-console.log(datos.procedencia);
-console.log(datos.formato);
-console.log(datos.tipoAudio);
-console.log(datos.narrador);
-console.log(datos.dura)
-}
+      datos.appendChild(div);
+      
+   });
+
+});
