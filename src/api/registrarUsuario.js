b9fb2fa4f4030708b8da6e6ed4d560edba42c23f
Kevin <kevindboy96@gmail.com>
2023-10-23 00:38:08 -0400

    actualizacion registrar usuario

--- src/api/registrarUsuario.js
+++ src/api/registrarUsuario.js
@@ -1,7 +1,51 @@
 document.getElementById("registroForm").addEventListener("submit", (e) => {
     e.preventDefault();
 });
-document.querySelectorAll("#registroForm input");
+const validationRules = {
+    nombre: {
+      minLength: 3,
+      maxLength: 20,
+      pattern: /^[A-Za-z]+$/,
+      errorElementId: "nombreError",
+      errorMessage: "El campo nombre debe tener entre 3 y 20 caracteres alfabéticos.",
+    },
+    apellido: {
+      minLength: 5,
+      maxLength: 20,
+      pattern: /^[A-Za-z]+$/,
+      errorElementId: "apellidoError",
+      errorMessage: "El campo apellido debe tener entre 5 y 20 caracteres alfabéticos.",
+    },
+    nombreDeUsuario: {
+      minLength: 4,
+      maxLength: 15,
+      pattern: /^[A-Za-z0-9]+$/,
+      errorElementId: "nombreDeUsuarioError",
+      errorMessage: "El campo nombre de usuario debe tener entre 4 y 15 caracteres alfanuméricos.",
+    },
+    correoElectronico: {
+      maxLength: 64,
+      pattern: /^[A-Za-z0-9]+@gmail\.com$/,
+      errorElementId: "correoElectronicoError",
+      errorMessage: "El campo correo electrónico debe contener caracteres alfabéticos y numéricos antes de @gmail.com.",
+    },
+    numeroCelular: {
+      maxLength: 8,
+      pattern: /^\d+$/,
+      errorElementId: "numeroCelularError",
+      errorMessage: "El campo número de celular debe tener hasta 8 caracteres numéricos.",
+    },
+    contrasena: {
+      maxLength: 32,
+      pattern: /^[A-Za-z0-9]+$/,
+      errorElementId: "contrasenaError",
+      errorMessage: "El campo contraseña debe tener hasta 32 caracteres alfanuméricos.",
+    },
+    repitaContrasena: {
+      errorElementId: "repitaContrasenaError",
+      errorMessage: "Las contraseñas no ",
+    },
+  };
 function registrarUsuario() {
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
@@ -16,63 +60,36 @@ function registrarUsuario() {
     db.where("nombreDeUsuario", "==", nombreDeUsuario).get()
     .then((querySnapshot) => {
         if (!querySnapshot.empty) {
-            alert("Nombre de usuario ya existe");
+            document.getElementById("nombreDeUsuarioError").innerText = "El nombre de usuario ya existe";
         }else {
-            // Validación: Campos vacíos
-            if (!nombre || !apellido || !correoElectronico || !nombreDeUsuario || !contrasena || !repitaContrasena || !numeroCelular) {
-                alert("Error espacios vacíos");
-                return;
-            }
-            // Validación: Tamaño nombre 
-            if (nombre.length < 3 || nombre.length > 20 || !/^[A-Za-z]+$/.test(nombre)) {
-                alert("El campo nombre debe tener entre 3 y 20 caracteres alfabéticos.");
-                return;
-            }
-            // Validación: Tamaño apellido
-            if (apellido.length < 5 || apellido.length > 20 || !/^[A-Za-z]+$/.test(apellido)) {
-                alert("El campo apellido debe tener entre 5 y 20 caracteres alfabéticos.");
-                return;
-            }
-            // Validación: Tamaño nombre de usuario
-            if (nombreDeUsuario.length < 4 || nombreDeUsuario.length > 15 || !/^[A-Za-z0-9]+$/.test(nombreDeUsuario)) {
-                alert("El campo nombre de usuario debe tener entre 4 y 15 caracteres alfanuméricos.");
-                return;
-            }
-            // Validación: Correo electrónico válido
-            if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(correoElectronico)) {
-                alert("Ingrese un correo electrónico válido.");
-                return;
-            }
-            // Validación: Tamaño correo electrónico
-            if (correoElectronico.length > 64) {
-                alert("El campo correo electrónico no debe exceder los 64 caracteres.");
-                return;
-              }
+            document.getElementById("nombreDeUsuarioError").innerText = "";
+            for (const field in validationRules) {
+                const value = document.getElementById(field).value;
+                const rule = validationRules[field];
             
-              // Validación: Correo electrónico tiene formato correcto
-            if (!/^[A-Za-z0-9]+@gmail\.com$/.test(correoElectronico)) {
-                alert("El campo correo electrónico debe contener caracteres alfabéticos y numéricos antes de @gmail.com.");
-                return;
+                // Elimina mensajes de error anteriores
+                document.getElementById(rule.errorElementId).innerText = "";
+            
+                // Realiza la validación
+                if (rule.minLength && value.length < rule.minLength) {
+                    document.getElementById(rule.errorElementId).innerText = rule.errorMessage;
+                    return;
+                }
+            
+                if (rule.maxLength && value.length > rule.maxLength) {
+                    document.getElementById(rule.errorElementId).innerText = rule.errorMessage;
+                    return;
+                }
+            
+                if (rule.pattern && !rule.pattern.test(value)) {
+                    document.getElementById(rule.errorElementId).innerText = rule.errorMessage;
+                    return;
+                }
             }
-            // Validación: Número de celular no excede los 8 caracteres y es numérico
-            if (numeroCelular.length != 8 || !/^\d+$/.test(numeroCelular)) {
-                alert("El campo número de celular debe tener 8 caracteres numéricos.");
-                return;
-            }
-            // Validación: Número de celular comienza con código de área (por ejemplo, 6 o 7)
-            if (!/^[67]/.test(numeroCelular)) {
-                alert("El primer carácter del número de celular debe ser 6 o 7.");
-                return;
-            }
-            // Validación: Contraseña no excede los 32 caracteres y permite caracteres alfanuméricos
-            if (contrasena.length < 8 || contrasena.length > 32 || !/^[A-Za-z0-9]+$/.test(contrasena)) {
-                alert("El campo contraseña debe tener entre 8 y 32 caracteres alfanuméricos.");
-                return;
-            }
-
-            // Validación: Repetir contraseña es igual a la contraseña
+            
+            // Validación de contraseña repetida
             if (contrasena !== repitaContrasena) {
-                alert("La contraseña y la repetición de contraseña no coinciden.");
+                document.getElementById(validationRules.repitaContrasena.errorElementId).innerText = validationRules.repitaContrasena.errorMessage;
                 return;
             }
             
