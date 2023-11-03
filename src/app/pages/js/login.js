0984ad4c998970a3219e501f18efa8b203e26896
Kevin <kevindboy96@gmail.com>
2023-11-02 21:09:10 -0400

    arreglo de conflictos

--- src/app/pages/js/login.js
+++ src/app/pages/js/login.js
@@ -16,7 +16,6 @@ loginForm.addEventListener("submit", function (e) {
         return;
     }
 
-
     // Validación de longitud de campos
     if (email.length > 64) {
         errorContainer.innerText = "El correo electrónico no debe exceder los 64 caracteres";
@@ -37,18 +36,18 @@ loginForm.addEventListener("submit", function (e) {
                 window.location.href = "./../../homeAdm/homeAdm.html";
             } else {
                 // Redirige a la página homeUsu.html para otros correos
-                window.location.href = "./../../homeUsu/homeUsu.html";
+                window.location.href = "./../../homeAdm/homeUsu.html";
             }
         })
         .catch((error) => {
-            // Manejo de errores de autenticación
-            if (error.code === "auth/user-not-found") {
+          // Maneja los errores de autenticación
+          if (error.code === "auth/user-not-found") {
               errorContainer.innerText = "El correo electrónico no existe";
-            } else if (error.code === "auth/wrong-password") {
+          } else if (error.code === "auth/wrong-password") {
               errorContainer.innerText = "Contraseña incorrecta";
-            } else {
+          } else {
               // Otros errores
-              errorContainer.innerText = "Error de inicio de sesión: ";
-            }
+              errorContainer.innerText = "Error de inicio de sesión";
+          }
         });
 });
