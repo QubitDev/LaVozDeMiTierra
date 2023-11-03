1957527c61559e84efa54fb5859666c9ae8bb62d
Yesica <sanchezmaria2c@gmail.com>
2023-11-03 12:51:29 -0400

    feat:nostrar texto y subir documentacion

--- src/app/pages/js/reproducir.js
+++ src/app/pages/js/reproducir.js
@@ -1,9 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
-const docId = urlParams.get("doc");
+const docId = urlParams.get("text-content");
 const docIdHome = urlParams.get("docHome");
 const contenedorCards = document.getElementById('card');
-
-console.log("id:: ",docId);
 
 const tipo = document.getElementById("tipo__audio");
 const  titulo = document.getElementById("titulo__audio");
@@ -11,7 +9,6 @@ const narradorAudio = document.getElementById("narrador");
 const musicaF = document.getElementById("musica");
 
 const audioElement = document.getElementById("audioE");
-const textURL={};
 const textContentElement = document.getElementById("text_content");
 
 
@@ -22,11 +19,10 @@ db.collection("audio").doc(docId).get().then((doc) => {
       titulo.innerText = data.titulo;
       narradorAudio.innerText = `Narrado por: ${data.narrador}`;
       musicaF.innerText = `Música de Fondo: ${data.musica}`;
+    
+      //textURL.URLt = data.textURL;
       
-
-      textURL.URLt = data.textURL;
-      
-      console.log("url: ",textURL);
+      //console.log("url: ",textURL);
       
       audioElement.src = data.audioURL;
     fetch(textURL.URLt)
@@ -51,9 +47,68 @@ db.collection("audio").doc(docId).get().then((doc) => {
 });
 
 
-console.log("url: ",textURL.URLt);
+//console.log("url: ",textURL.URLt);
+/*
+if(doc){
+  showFile(doc);
+}
+
+function showFile(file){
+   fetch(`./assets/${documentacion}.txt`).then((response) =>{
+      if(!response.ok){
+        throw new Error(`Error en la solicitud : ${response.status}`);
+      }
+      return response.text();
+   })
+   .then((data) =>{
+      contentMain.innerHTML=data;
+      loadCSS(file);
+      loadJS(file);
+   })
+   .catch((error) =>{
+     console.error("Error al cargar el contenido:", error);
+   });
+
+}
+
+function loadCSS(file){
+  const link = document.createElement("link");
+  link.rel="stylesheet";
+  link.type="text/css";
+  link.href = './css/${file}.css';
+}
+
+fetch("./assets/documentacion/El abuelo y el raton.txt").then(response=> response.text().then(TEXT => writeToConsole(TEXT));
+function writeToConsole(TEXT){
+  console.log(TEXT);
+}*/
+
+const fs = require('fs');
+const path = require('path');
+
+// Construct the file path to El origen del guajojo.txt
+const filePath = path.join(__dirname, 'assets', 'documentacion', 'El origen del guajojo.txt');
+
+fs.readFile(filePath, (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data.toString());
+    }
+});
 
 
+
+
+
+/*
+
+const fs = require('fs')
+        fs.readFile('El origen del guajojo.txt', (err, inputD) => {
+           if (err) throw err;
+              console.log(inputD.toString());
+        })
+*/
 //barra lateral
 
 
