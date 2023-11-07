9e5c9646df3f44c5df6b5241843f73e7f289d668
Gabo90002023 <145948188+Gabo90002023@users.noreply.github.com>
2023-11-07 08:12:46 -0400

    Update homeUsu.js
    Parte del usuario  cargar de cuentos y leyendas

--- src/app/pages/js/homeUsu.js
+++ src/app/pages/js/homeUsu.js
@@ -2,27 +2,16 @@ const contenedorCards = document.getElementById('card');
 const botonAnterior = document.getElementById('botonAnterior');
 const botonSiguiente = document.getElementById('botonSiguiente');
 
-let ultimoDoc = null;
-let primerDoc = null;
-
-
-
 
 db.collection('audio').onSnapshot((snapshot) => {
-
-
     cargarDocumentoCuento(snapshot.docs);
-
 });
 
 const iddoc = {};
 
 const cargarDocumentoCuento = (documentos) => {
     if (documentos.length > 0) {
-        // ultimoDoc = documentos[documentos.length - 1];
-        // primerDoc = documentos[0];
-
-        // contenedorCards.innerHTML = '';
+        
         documentos.forEach(documento => {
             if (documento.data().tipoAudio == "Cuento") {
                 contenedorCards.innerHTML += `
@@ -52,7 +41,6 @@ db.collection('audio').onSnapshot((snapshot) => {
 
     cargarDocumentoLeyenda(snapshot.docs);
 });
-// leyenda
 const contenedorCards1 = document.getElementById('card1');
 const cargarDocumentoLeyenda = (documentos) => {
     if (documentos.length > 0) {
@@ -62,22 +50,17 @@ const cargarDocumentoLeyenda = (documentos) => {
                 contenedorCards1.innerHTML += `
             <div class="carta" id="carta" onClick="enviar('${documento.id}')">
                 <figure>
-					<img src="${documento.data().imageURL}"
-						alt="La-leyenda-de-la-quinua-y-la-sal">
-				</figure>
-				
-				<div class="contenido-card">
-					<h3>${documento.data().titulo}</h3>
-					<p>${documento.data().musica}</p>
-
-				</div>
-
+			<img src="${documento.data().imageURL}"
+				alt="La-leyenda-de-la-quinua-y-la-sal">
+		</figure>
+		<div class="contenido-card">
+			<h3>${documento.data().titulo}</h3>
+			<p>${documento.data().musica}</p>
+		</div>
             </div>
             `;
-            }
-
+           }
         });
-
     }
 }
 
