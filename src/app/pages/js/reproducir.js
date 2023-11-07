643242f3713784cb9b90c3416070e1b7cf0792c4
Yesica <sanchezmaria2c@gmail.com>
2023-11-06 23:31:22 -0400

    feat:codificar ejemplos de texto

--- src/app/pages/js/reproducir.js
+++ src/app/pages/js/reproducir.js
@@ -1,6 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
-const docId = urlParams.get("text-content");
-const docIdHome = urlParams.get("docHome");
+const docId= urlParams.get("id-doc");
+const texto = urlParams.get("text-content");
+//const docIdHome = urlParams.get("docHome");
 const contenedorCards = document.getElementById('card');
 
 const imagenC = documento.getElementById("imagen__C")
@@ -10,7 +11,72 @@ const narradorAudio = document.getElementById("narrador");
 const musicaF = document.getElementById("musica");
 
 const audioElement = document.getElementById("audioE");
-const textContentElement = document.getElementById("text_content");
+const uploadedfiles = [];
+
+if(texto){
+  showFile(texto);
+}
+
+
+function showFile(file) {
+
+	console.log("pant = ",pantallaActual)
+	if (file === "home") {
+		window.location.reload();
+		window.location.reload();
+		if (user === "homeUsu") {
+		  file = "homeUsu"; 
+		} else if (user === "homeAdm") {
+		  file = "homeAdm";
+		}
+	}
+
+	if (file === pantallaActual) {
+		return; 
+	}
+	// removeScript(file);
+	
+	  pantallaActual = file; 
+	
+	  contentMain.innerHTML = "";
+
+	
+	  fetch(`./assets/documentcion/${file}.txt`)
+		.then((response) => {
+		  if (!response.ok) {
+			throw new Error(`Error en la solicitud: ${response.status}`);
+		  }
+		  return response.text();
+		})
+		  .then((data) => {
+		  contentMain.innerHTML = data;
+		})
+		.catch((error) => {
+		  console.error("Error al cargar el contenido:", error);
+		});
+	
+	if (!uploadedfiles.includes(file)) {
+	  	loadJS(file);
+		loadCSS(file);
+		uploadedfiles.push(file);
+	}
+	
+}
+
+function loadCSS(file) {
+	const link = document.createElement("link");
+	link.rel = "stylesheet";
+	link.type = "text/css";
+	link.href = `./css/${file}.css`; 
+	document.head.appendChild(link);
+}
+  
+function loadJS(file) {
+	const script = document.createElement("script");
+	script.src = `./js/${file}.js`; 
+	document.body.appendChild(script);
+}
+  
 
 imagenC.addEventListener("change", function () {
   const selectedImage = this.files[0]; // Obtén el archivo de imagen seleccionado
@@ -39,83 +105,27 @@ db.collection("audio").doc(docId).get().then((doc) => {
       titulo.innerText = data.titulo;
       narradorAudio.innerText = `Narrado por: ${data.narrador}`;
       musicaF.innerText = `Música de Fondo: ${data.musica}`;
-    
+      //const textContentElement = document.getElementById("text-content");
+      //textContentElement.innerText = data.textURL;
       //textURL.URLt = data.textURL;
       
       //console.log("url: ",textURL);
       
       audioElement.src = data.audioURL;
-      
-      textContentElement.src = data.textURL;
-      Imagen.src = data.imageURL;      
+      //textContentElement.innerText = data.textURL;
+      //Imagen.src = data.imageURL;      
   } else {
       console.log("No se encontró el documento en Firestore.");
   }
 });
 
 
-//console.log("url: ",textURL.URLt);
-/*
-if(doc){
-  showFile(doc);
-}
-
-function showFile(file){
-   fetch(`./assets/${documentacion}.txt`).then((response) =>{
-      if(!response.ok){
-        throw new Error(`Error en la solicitud : ${response.status}`);
-      }
-      return response.text();
-   })
-   .then((data) =>{
-      contentMain.innerHTML=data;
-      loadCSS(file);
-      loadJS(file);
-   })
-   .catch((error) =>{
-     console.error("Error al cargar el contenido:", error);
-   });
-
-}
-
-function loadCSS(file){
-  const link = document.createElement("link");
-  link.rel="stylesheet";
-  link.type="text/css";
-  link.href = './css/${file}.css';
-}
-
-fetch("./assets/documentacion/El abuelo y el raton.txt").then(response=> response.text().then(TEXT => writeToConsole(TEXT));
-function writeToConsole(TEXT){
-  console.log(TEXT);
-}*/
-
-const fs = require('fs');
-const path = require('path');
-
-// Construct the file path to El origen del guajojo.txt
-const filePath = path.join(__dirname, 'assets', 'documentacion', 'El origen del guajojo.txt');
-
-fs.readFile(filePath, (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data.toString());
-    }
-});
-
-
-
-
-
-/*
-
 const fs = require('fs')
         fs.readFile('El origen del guajojo.txt', (err, inputD) => {
            if (err) throw err;
               console.log(inputD.toString());
         })
-*/
+
 //barra lateral
 
 
