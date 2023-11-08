ac2eab01df2b4d1c1a1d466731c789de3e44e1d1
Kevin <kevindboy96@gmail.com>
2023-11-08 00:33:59 -0400

    arreglo de errores reportados

--- src/app/users/js/Recuperar.js
+++ src/app/users/js/Recuperar.js
@@ -8,6 +8,7 @@ document.getElementById("reset-password-form").addEventListener("submit", functi
     .then(function () {
       // Solicitud de restablecimiento de contraseña enviada con éxito
       alert("Se ha enviado un correo electrónico para restablecer tu contraseña. Revise su bandeja de entrada.");
+      window.location.href = "Login.html";
     })
     .catch(function (error) {
       // Ocurrió un error al enviar la solicitud de restablecimiento de contraseña
