090d8f7e67ec38f44890523b1596d88af28d68a9
EncinasDev <encinas.jhonatan.dev@gmail.com>
2023-11-12 17:15:00 -0400

    feat: realizar front-end para compartir

--- src/app/pages/js/share.js
+++ src/app/pages/js/share.js
@@ -1,27 +1,22 @@
-function generarEnlace() {
-    const contenido = () => { 
-        return "Este es el contenido del cuento o leyenda...";
-    }
-    
-    var cuentoID = () => { 
-         return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
 
-    // Crear un enlace único para compartir
-    var enlaceCompartir = window.location.origin + "/reproducir?cuento=" + cuentoID;
+function generarEnlaceCompartir() {
+    // Obtén los datos relevantes que deseas compartir (pueden ser el título, el narrador, etc.)
+    var tipoAudio = document.getElementById("tipo__audio").textContent;
+    var tituloAudio = document.getElementById("titulo__audio").textContent;
 
-    // Mostrar el enlace para compartir (puedes usar una modal, alert, o cualquier otro método)
+    // Simula la generación de un ID único para el contenido que se está reproduciendo
+    var contenidoID = generarIDUnico();
+
+    // Construye el enlace con la información necesaria
+    var enlaceCompartir = window.location.origin + "/reproducir.html?contenido=" + contenidoID;
+
+    // Muestra el enlace para compartir (puedes usar una modal, alert, o cualquier otro método)
     alert("Comparte este enlace:\n" + enlaceCompartir);
 }
 
-function obtenerContenido() {
-    // Aquí obtienes el contenido del cuento desde tu aplicación
-    // Puedes usar una variable, una llamada AJAX, o cualquier método según tu implementación
-    
+// Función para generar un ID único (puedes mejorarla según tus necesidades)
+function generarIDUnico() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
-function generarIDUnico() {
-    // Generar un ID único para el cuento
-    // Puedes usar bibliotecas como UUID o cualquier método que prefieras
-   
-}
+// ... (Más código, según sea necesario)
