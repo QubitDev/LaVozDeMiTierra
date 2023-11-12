2159ca32cf754cb8b85aee63452535ba4e9d3263
EncinasDev <encinas.jhonatan.dev@gmail.com>
2023-11-12 19:06:57 -0400

    feat: Realizar backent de share

--- src/app/pages/js/share.js
+++ src/app/pages/js/share.js
@@ -1,22 +1,38 @@
+// document.getElementById("buttonCancel").addEventListener("click", onCancel);
+// document.getElementById("buttonCopy").addEventListener("click", copyToClipboard);
+const popup = document.getElementById('popup');
+const linkShare = document.getElementById("__link__share");
+const input = document.createElement("textarea");
 
-function generarEnlaceCompartir() {
+function togglePopup() {
+    popup.style.display = (popup.style.display === "none" || popup.style.display === "") ? "block" : "none";
+    generateLinkShare();
+}
+
+function generateLinkShare() {
     // Obtén los datos relevantes que deseas compartir (pueden ser el título, el narrador, etc.)
-    var tipoAudio = document.getElementById("tipo__audio").textContent;
-    var tituloAudio = document.getElementById("titulo__audio").textContent;
+    var typeAudio = document.getElementById("tipo__audio").textContent;
+    var titleAudio = document.getElementById("titulo__audio").textContent;
 
     // Simula la generación de un ID único para el contenido que se está reproduciendo
-    var contenidoID = generarIDUnico();
+    var contentID = generateUniqueID();
 
     // Construye el enlace con la información necesaria
-    var enlaceCompartir = window.location.origin + "/reproducir.html?contenido=" + contenidoID;
+    var shareLink = window.location.origin + "./../html/reproducir.html?contenido=" + contentID;
 
-    // Muestra el enlace para compartir (puedes usar una modal, alert, o cualquier otro método)
-    alert("Comparte este enlace:\n" + enlaceCompartir);
+    linkShare.innerText = shareLink;
 }
 
 // Función para generar un ID único (puedes mejorarla según tus necesidades)
-function generarIDUnico() {
+function generateUniqueID() {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
-// ... (Más código, según sea necesario)
+function copyToClipboard() {
+ 
+    input.value = linkShare.innerText;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("copy");
+    document.body.removeChild(input);
+  }
