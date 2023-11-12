eaa2a8b6aa85813b84943f7b7e826836791e59a8
Yesica <sanchezmaria2c@gmail.com>
2023-11-12 12:22:28 -0400

    feat: realizar fronted ranking

---
+++ src/app/pages/js/ranking.js
@@ -0,0 +1,12 @@
+const endSesion = document.querySelector(".sesion");
+endSesion.addEventListener('click',cerrarSesion);
+let cont = 1;
+function cerrarSesion(){
+    if(cont % 2 == 0){
+        document.getElementById('sesionMenu').style.display= 'none';
+    }
+    else{
+        document.getElementById('sesionMenu').style.display= 'block';
+    }
+    cont++;
+}
