69758d5d7d6e69cc6e092826d57bcd069233146d
EncinasDev <encinas.jhonatan.dev@gmail.com>
2023-11-18 01:01:29 -0400

    feat: Reparar cierre de cesion

--- src/app/pages/js/reproducir.js
+++ src/app/pages/js/reproducir.js
@@ -11,6 +11,21 @@ const musicaF = document.getElementById("musica");
 const audioElement = document.getElementById("audioE");
 const textContentElement = document.getElementById("text_content");
 
+const endSesion = document.querySelector(".sesion");
+endSesion.addEventListener('click',cerrarSesion);
+
+let cont = 1;
+function cerrarSesion(){
+    if(cont % 2 == 0){
+        document.getElementById('sesionMenu').style.display= 'none';
+    }
+    else{
+        document.getElementById('sesionMenu').style.display= 'block';
+    }
+    cont++;
+}
+
+
 db.collection("audio").doc(receivedArray[0]).get().then((doc) => {
   if (doc.exists) {
       tipo.innerText = doc.data().tipoAudio;
