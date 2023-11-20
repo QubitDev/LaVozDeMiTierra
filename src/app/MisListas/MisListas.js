c1b6f869bf0655690fd6b49b5a2340941afff5e2
Gabriel <117124925+Gabriel-Cabero-Torrico@users.noreply.github.com>
2023-11-20 19:43:14 -0400

    feat: Diseño de las listas del usuario

--- src/app/MisListas/MisListas.js
+++ src/app/MisListas/MisListas.js
@@ -1,7 +1,6 @@
 const endSesion = document.querySelector(".sesion");
 endSesion.addEventListener('click',cerrarSesion);
-const showList = document.querySelectorAll(".imageList");
-showList.addEventListener("click", showListL);
+
 let cont = 1;
 function cerrarSesion(){
     if(cont % 2 == 0){
@@ -19,18 +18,22 @@ let number = 1;
 function addLista(){
     conteiner.innerHTML +=`
     <div class="list">
-        <i class="fa-solid fa-headphones fa-3x" class="imageList"></i>
+        <div class = "imageList">
+            <i class="fa-solid fa-headphones fa-3x" class="imageList"></i>
+        </div>
         <h1 class="titleLi">Lista numero ${number}</h1>
         <h1 class="Audios">Numero de audios:</h1>
         <h1 class="number" type="number">0</h1>
         <button class="deleteC">
-            <i class="fa-solid fa-trash-can fa-2x"></i>
+            <i class="fa-solid fa-trash-can fa-2x" class="deletedLis"></i>
         </button>
     </div>
     
     `;
     number++;
 }
+const showList = document.querySelectorAll(".imageList");
+showList.addEventListener("click", showListL);
 function showListL(){
     document.getElementById("contenedor").style.display = 'none';
     document.getElementById("contenedor_Lista").style.display = 'block';
