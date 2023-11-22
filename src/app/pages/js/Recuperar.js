a4f07365afe87270ac2ec542d7226a8c0617625b
Gabriel <117124925+Gabriel-Cabero-Torrico@users.noreply.github.com>
2023-11-22 00:23:28 -0400

    feat: Cambios en el registro de usaurio

--- src/app/pages/js/Recuperar.js
+++ src/app/pages/js/Recuperar.js
@@ -1,18 +1,17 @@
-
 document.getElementById("reset-password-form").addEventListener("submit", function (e) {
-    e.preventDefault(); // Previene el envío del formulario
-  
-    const email = document.getElementById("email").value;
-  
-    // Envía la solicitud de restablecimiento de contraseña a Firebase
-    firebase.auth().sendPasswordResetEmail(email)
-      .then(function () {
-        // Solicitud de restablecimiento de contraseña enviada con éxito
-        alert("Se ha enviado un correo electrónico para restablecer tu contraseña. Revise su bandeja de entrada.");
-        window.location.href = "Login.html";
-      })
-      .catch(function (error) {
-        // Ocurrió un error al enviar la solicitud de restablecimiento de contraseña
-        alert("Ha ocurrido un error: " + error.message);
-      });
-  });
+  e.preventDefault(); // Previene el envío del formulario
+
+  const email = document.getElementById("email").value;
+
+  // Envía la solicitud de restablecimiento de contraseña a Firebase
+  firebase.auth().sendPasswordResetEmail(email)
+    .then(function () {
+      // Solicitud de restablecimiento de contraseña enviada con éxito
+      alert("Se ha enviado un correo electrónico para restablecer tu contraseña. Revise su bandeja de entrada.");
+      window.location.href = "Login.html";
+    })
+    .catch(function (error) {
+      // Ocurrió un error al enviar la solicitud de restablecimiento de contraseña
+      alert("Ha ocurrido un error: " + error.message);
+    });
+});
