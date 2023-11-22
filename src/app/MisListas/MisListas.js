4adb53bc6c02da35337278828d3064059796ca38
Gabriel <117124925+Gabriel-Cabero-Torrico@users.noreply.github.com>
2023-11-21 23:11:19 -0400

    feat: Cambio en la image de la presentacion

--- src/app/MisListas/MisListas.js
+++ src/app/MisListas/MisListas.js
@@ -19,7 +19,7 @@ function addLista(){
     conteiner.innerHTML +=`
     <div class="list">
         <div class = "imageList">
-            <i class="fa-solid fa-headphones fa-3x" class="imageLC"></i>
+            <i class="fa-solid fa-headphones fa-3x" class="imageList"></i>
         </div>
         <h1 class="titleLi">Lista numero ${number}</h1>
         <h1 class="Audios">Numero de audios:</h1>
@@ -27,8 +27,7 @@ function addLista(){
         <button class="deleteC">
             <i class="fa-solid fa-trash-can fa-2x" class="deletedLis"></i>
         </button>
-    </div>
-    
+    </div>    
     `;
     number++;
 }
@@ -37,4 +36,76 @@ showList.addEventListener("click", showListL);
 function showListL(){
     document.getElementById("contenedor").style.display = 'none';
     document.getElementById("contenedor_Lista").style.display = 'block';
+}
+
+// Referencia a las colecciones 'audio' y 'playlist'
+const audioCollection = db.collection("audio");
+const playlistCollection = db.collection("playlist");
+
+// Función para cargar y mostrar los audios disponibles
+function cargarAudiosDisponibles() {
+    const audioListElement = document.getElementById('audioList');
+
+    audioCollection.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        const audio = doc.data();
+        const audioItem = document.createElement('div');
+        audioItem.classList.add('audio-item');
+        audioItem.innerHTML = `
+            <input type="checkbox" id="audio_${doc.id}">
+            <label for="audio_${doc.id}">${audio.titulo}</label>
+            `;
+            audioListElement.appendChild(audioItem);
+        });
+    }).catch((error) => {
+        console.error("Error al obtener audios:", error);
+    });
+}
+
+
+
+
+// Llama a la función para cargar los audios cuando se carga la página
+document.addEventListener("DOMContentLoaded", cargarAudiosDisponibles);
+
+// Función para crear una playlist con los audios seleccionados
+function crearPlaylist() {
+    const playlistName = document.getElementById('playlistName').value;
+
+    if (!playlistName) {
+        alert("Por favor, ingrese un nombre para la playlist.");
+        return;
+    }
+
+    const selectedAudios = [];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    checkboxes.forEach((checkbox) => {
+        const audioId = checkbox.id.split('_')[1];
+        selectedAudios.push(audioId);
+    });
+
+    if (selectedAudios.length === 0) {
+        alert("Seleccione al menos un audio para la playlist.");
+        return;
+    }
+
+    // Obtiene el usuario actualmente autenticado
+    const user = auth.currentUser;
+
+    if (user) {
+        // Crea la playlist en la base de datos
+        playlistCollection.add({
+          nombre: playlistName,
+          userId: user.uid,
+          audios: selectedAudios
+        })
+        .then(() => {
+          alert("Playlist creada con éxito.");
+        })
+        .catch((error) => {
+          console.error("Error al crear la playlist:", error);
+        });
+    } else {
+        alert("Usuario no autenticado. Inicie sesión para crear una playlist.");
+    }
 }
