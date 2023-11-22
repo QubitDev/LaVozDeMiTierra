b19d5528d87e005ef2b089a0bc83b3ea7b1cd1e8
MiguelRodriguez123 <miguelrodriguezrobles23@gmail.com>
2023-11-22 08:19:42 -0400

    fead:ver la lista de favorito

--- src/app/pages/js/favoritos.js
+++ src/app/pages/js/favoritos.js
@@ -11,3 +11,44 @@ function cerrarSesion(){
     }
     cont++;
 }
+
+/* --------------------- la lista de los favoritos ----------------------------------------------------*/ 
+db.collection('audio').onSnapshot((snapshot) => {
+    cargarDocumentoCuento(snapshot.docs);
+});
+
+const cargarDocumentoCuento = (documentos) => {
+    const contenedorCards = document.getElementById('card');
+    if (documentos.length > 0) {
+        
+        documentos.forEach(documento => {
+            if (/*documento.data().tipoAudio == "Cuento" && */ contenedorCards !== null) {
+                contenedorCards.innerHTML += `
+                <div class="contenedora">
+                <div class="contenedor_div">
+                    <img src="${documento.data().imageURL}">
+                </div>
+            
+                <div class="contenedor_div">
+                    <h2>${documento.data().titulo}</h2>
+                 </div>
+                 <div class="contenedor_div">
+                    <h2>${documento.data().procedencia}</h2>
+                 </div>
+                 <div class="contenedor_div">
+                    <h2>${documento.data().narrador}</h2>
+                 </div>
+                 <div class="contenedor_div">
+                    <h2>${documento.data().duracion}</h2>
+                 </div>
+                 <div class="contenedor_div">
+                    <h2>♥️</h2>
+                 </div>
+            </div>
+            
+                `;
+            }
+        });
+
+    }
+}
