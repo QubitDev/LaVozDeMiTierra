80219631da516409b8b07480b527c2348343fcdf
Kevin <kevindboy96@gmail.com>
2023-11-24 00:35:33 -0400

    arreglos

--- src/app/MisListas/MisListas.js
+++ src/app/MisListas/MisListas.js
@@ -1,13 +1,7 @@
 const endSesion = document.querySelector(".sesion");
 endSesion.addEventListener('click', cerrarSesion);
-endSesion.addEventListener('click', cerrarSesion);
 
 let cont = 1;
-function cerrarSesion() {
-    if (cont % 2 == 0) {
-        document.getElementById('sesionMenu').style.display = 'none';
-    } else {
-        document.getElementById('sesionMenu').style.display = 'block';
 function cerrarSesion() {
     if (cont % 2 == 0) {
         document.getElementById('sesionMenu').style.display = 'none';
