3f583d569ffa925726435f8e8444285672d37299
Kevin <kevindboy96@gmail.com>
2023-11-23 22:33:25 -0400

    listas

--- src/app/MisListas/MisListas.js
+++ src/app/MisListas/MisListas.js
@@ -1,41 +1,52 @@
 const endSesion = document.querySelector(".sesion");
-endSesion.addEventListener('click',cerrarSesion);
+endSesion.addEventListener('click', cerrarSesion);
 
 let cont = 1;
-function cerrarSesion(){
-    if(cont % 2 == 0){
-        document.getElementById('sesionMenu').style.display= 'none';
-    }
-    else{
-        document.getElementById('sesionMenu').style.display= 'block';
+function cerrarSesion() {
+    if (cont % 2 == 0) {
+        document.getElementById('sesionMenu').style.display = 'none';
+    } else {
+        document.getElementById('sesionMenu').style.display = 'block';
     }
     cont++;
 }
+
 const addList = document.getElementById("newList");
 const conteiner = document.getElementById("container_list");
-addList.addEventListener("click",addLista);
+addList.addEventListener("click", addLista);
 let number = 1;
-function addLista(){
-    conteiner.innerHTML +=`
+
+function addLista() {
+    conteiner.innerHTML += `
     <div class="list">
-        <div class = "imageList">
-            <i class="fa-solid fa-headphones fa-3x" class="imageList"></i>
+        <div class="imageList">
+            <i class="fa-solid fa-headphones fa-3x"></i>
         </div>
-        <h1 class="titleLi">Lista numero ${number}</h1>
-        <h1 class="Audios">Numero de audios:</h1>
+        <h1 class="titleLi" contenteditable="true">Lista número ${number}</h1>
+        <h1 class="Audios">Número de audios:</h1>
         <h1 class="number" type="number">0</h1>
-        <button class="deleteC">
-            <i class="fa-solid fa-trash-can fa-2x" class="deletedLis"></i>
+        <button class="deleteC" onclick="eliminarLista(this)">
+            <i class="fa-solid fa-trash-can fa-2x"></i>
         </button>
     </div>    
     `;
     number++;
 }
+
+function eliminarLista(button) {
+    const listItem = button.parentElement;
+    listItem.remove();
+}
+
 const showList = document.querySelectorAll(".imageList");
-showList.addEventListener("click", showListL);
-function showListL(){
-    document.getElementById("contenedor").style.display = 'none';
-    document.getElementById("contenedor_Lista").style.display = 'block';
+
+// Recorre cada elemento de imagen y agrega un event listener de clic
+showList.forEach(image => {
+    image.addEventListener("click", showListL);
+});
+
+function showListL() {
+    window.location.href = 'lista.html';
 }
 /*
 const db = firebase.firestore();
