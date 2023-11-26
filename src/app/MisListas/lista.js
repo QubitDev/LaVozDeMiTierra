cbcbfe1335f44c6d37430a8d7d3dd5b91cd8f327
Kevin <kevindboy96@gmail.com>
2023-11-26 17:06:21 -0400

    listas casi concluido

--- src/app/MisListas/lista.js
+++ src/app/MisListas/lista.js
@@ -28,21 +28,24 @@ playlistCollection.get().then((querySnapshot) => {
       const audioDoc = await db.collection("audio").doc(audioId).get();
 
       if (audioDoc.exists) {
-        const audio = audioDoc.data();
+        const audioData = audioDoc.data();
         const audioItem = document.createElement("div");
         audioItem.classList.add("audio-item");
         audioItem.innerHTML = `
-        <div class="imagen" id="imagen${index}>
-            <img src="${audio.imageURL}" alt="" height="100px" width="100px" class="imageF">
-        </div>
-        <h3 class="part1" id="titulo${index}">${audio.titulo}</h3>
-        <h4 class="part2" id="Cultura">${audio.procedencia}</h4>
-        <h5 class="part3" id="narrador">${audio.narrador}</h5>
-        <h6 class="part4" id="duracion">${audio.duracion}</h6>
-        <button class="deleteC">
-            <i class="fas fa-trash-can fa-2x" id="deleteLis"></i>
-       </button> 
+          <div class="contenido" id="contenido">
+          <a onclick="enviar('${doc.id}')">
+            <figure class="image"><img src="${audioData.imageURL}" width="80px" height="80px" id="imagenLis"></figure>
+          </a>
+            <p class="part1" id="titulo">${audioData.titulo}</p>
+            <p class="part2" id="Cultura">${audioData.procedencia}</p>
+            <p class="part3" id="narrador">${audioData.narrador}</p>
+            <p class="part4" id="duracion">${audioData.duracion}</p>
+            <button class="deleteC">
+                <i class="fas fa-trash-can fa-2x" id="deleteLis"></i>
+            </button>
+          </div>
         `;
+
         audioList.appendChild(audioItem);
       } else {
         console.error(`El documento de audio con ID ${audioId} no existe.`);
@@ -52,3 +55,7 @@ playlistCollection.get().then((querySnapshot) => {
 }).catch((error) => {
   console.error("Error al obtener datos de Firebase:", error);
 });
+
+function enviar(doc) {
+    window.location.href = `./reproducir.html?doc=${doc}`;
+  }
