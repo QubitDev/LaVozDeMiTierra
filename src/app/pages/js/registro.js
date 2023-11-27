9be9f2ba971c8c4173fd01f568ec811e5db5ecdc
EncinasDev <encinas.jhonatan.dev@gmail.com>
2023-11-27 19:08:36 -0400

    feat: mejorar apariencia de registro

--- src/app/pages/js/registro.js
+++ src/app/pages/js/registro.js
@@ -1,5 +1,21 @@
 
+const container = document.querySelector('.container__body');
+const rain = () => {
+    let j = 0;
+    while (j <= 80){
+        let gout = document.createElement('o');
+        let x = innerWidth * Math.random();
+        let time = 1 * Math.random();
+        gout.style.animationDuration = time <= 0.4 ? (time + 0.4) + 's'  : time + 's';
+        gout.style.animationDelay = time + 's';
+        gout.style.left = x + 'px';
+        container.appendChild(gout);
+        j++;
+    }
+}
+rain();
 
+//*********************************************** REGISTRO AUDIO ********************************************* */ 
 document.getElementById("cancelButton").addEventListener("click", onCancel);
 document.getElementById("submitButton").addEventListener("click", onSubmit);
 document.getElementById("okButton").addEventListener("click",closePopup);
