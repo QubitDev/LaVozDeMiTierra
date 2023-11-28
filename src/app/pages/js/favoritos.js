2c3b544f76d208a541370334f7884d11e2dfb5bc
MiguelRodriguez123 <miguelrodriguezrobles23@gmail.com>
2023-11-28 09:39:21 -0400

    merrge repositorio remoto

--- src/app/pages/js/favoritos.js
+++ src/app/pages/js/favoritos.js
@@ -1,21 +1,4 @@
 
-const container = document.querySelector('.contenedor_todo');
-const rain = () => {
-    let j = 0;
-    while (j <= 80){
-        let gout = document.createElement('o');
-        let x = innerWidth * Math.random();
-        let time = 1 * Math.random();
-        gout.style.animationDuration = time <= 0.4 ? (time + 0.4) + 's'  : time + 's';
-        gout.style.animationDelay = time + 's';
-        gout.style.left = x + 'px';
-        container.appendChild(gout);
-        j++;
-    }
-}
-rain();
-
-
 const endSesion = document.querySelector(".sesion");
 endSesion.addEventListener('click',cerrarSesion);
 
